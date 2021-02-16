fb6536c43176f987ab10c95a24b4e3ba09650906
Ives <ivesfurtado@yahoo.com>
2021-02-16 21:14:16 -0200

    Minor updates

--- datavue/app.js
+++ datavue/app.js
@@ -1,192 +1,14 @@
+axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+
 new Vue({
     el: '#app',
     data: {
-        products: [
-           {
-              "id": 1,
-              "name":"Intelligent Granite Table",
-              "category":"Tools",
-              "price":"787.00"
-           },
-           {
-              "id": 2,
-              "name":"Handcrafted Rubber Hat",
-              "category":"Games",
-              "price":"232.00"
-           },
-           {
-              "id": 3,
-              "name":"Rustic Concrete Salad",
-              "category":"Jewelery",
-              "price":"115.00"
-           },
-           {
-              "id": 4,
-              "name":"Gorgeous Concrete Pizza",
-              "category":"Garden",
-              "price":"250.00"
-           },
-           {
-              "id": 5,
-              "name":"Refined Plastic Shoes",
-              "category":"Health",
-              "price":"844.00"
-           },
-           {
-              "id": 6,
-              "name":"Awesome Metal Soap",
-              "category":"Tools",
-              "price":"326.00"
-           },
-           {
-              "id": 7,
-              "name":"Intelligent Fresh Mouse",
-              "category":"Home",
-              "price":"783.00"
-           },
-           {
-              "id": 8,
-              "name":"Licensed Soft Keyboard",
-              "category":"Music",
-              "price":"361.00"
-           },
-           {
-              "id": 9,
-              "name":"Fantastic Rubber Pants",
-              "category":"Garden",
-              "price":"786.00"
-           },
-           {
-              "id": 10,
-              "name":"Awesome Rubber Ball",
-              "category":"Automotive",
-              "price":"696.00"
-           },
-           {
-              "id": 11,
-              "name":"Handcrafted Soft Pizza",
-              "category":"Health",
-              "price":"31.00"
-           },
-           {
-              "id": 12,
-              "name":"Practical Soft Chips",
-              "category":"Computers",
-              "price":"795.00"
-           },
-           {
-              "id": 13,
-              "name":"Practical Frozen Shirt",
-              "category":"Kids",
-              "price":"879.00"
-           },
-           {
-              "id": 14,
-              "name":"Unbranded Plastic Car",
-              "category":"Toys",
-              "price":"454.00"
-           },
-           {
-              "id": 15,
-              "name":"Handcrafted Plastic Table",
-              "category":"Shoes",
-              "price":"189.00"
-           },
-           {
-              "id": 16,
-              "name":"Intelligent Plastic Car",
-              "category":"Grocery",
-              "price":"202.00"
-           },
-           {
-              "id": 17,
-              "name":"Ergonomic Wooden Pizza",
-              "category":"Electronics",
-              "price":"801.00"
-           },
-           {
-              "id": 18,
-              "name":"Refined Rubber Pants",
-              "category":"Home",
-              "price":"580.00"
-           },
-           {
-              "id": 19,
-              "name":"Small Frozen Hat",
-              "category":"Music",
-              "price":"654.00"
-           },
-           {
-              "id": 20,
-              "name":"Unbranded Cotton Chips",
-              "category":"Tools",
-              "price":"305.00"
-           },
-           {
-              "id": 21,
-              "name":"Unbranded Plastic Chicken",
-              "category":"Baby",
-              "price":"943.00"
-           },
-           {
-              "id": 22,
-              "name":"Rustic Fresh Pizza",
-              "category":"Toys",
-              "price":"647.00"
-           },
-           {
-              "id": 23,
-              "name":"Ergonomic Metal Tuna",
-              "category":"Industrial",
-              "price":"51.00"
-           },
-           {
-              "id": 24,
-              "name":"Unbranded Frozen Chicken",
-              "category":"Movies",
-              "price":"248.00"
-           },
-           {
-              "id": 25,
-              "name":"Ergonomic Cotton Table",
-              "category":"Baby",
-              "price":"4.00"
-           },
-           {
-              "id": 26,
-              "name":"Handmade Frozen Pants",
-              "category":"Home",
-              "price":"731.00"
-           },
-           {
-              "id": 27,
-              "name":"Rustic Cotton Bike",
-              "category":"Jewelery",
-              "price":"161.00"
-           },
-           {
-              "id": 28,
-              "name":"Licensed Metal Bacon",
-              "category":"Books",
-              "price":"331.00"
-           },
-           {
-              "id": 29,
-              "name":"Practical Cotton Soap",
-              "category":"Books",
-              "price":"861.00"
-           },
-           {
-              "id": 30,
-              "name":"Sleek Frozen Tuna",
-              "category":"Electronics",
-              "price":"375.00"
-           }
-        ],
+        products: [],
+        categories: [],
 
         order: {
            dir: 1,
-           column: 'price'
+           column: 'name'
         },
 
         filters: {
@@ -199,11 +21,25 @@ new Vue({
         product: {
            id: null,
            name: '',
-           category: '',
+           category_id: '',
            price: ''
         },
 
-        isEdit: false
+        isEdit: false,
+
+        errors: {},
+
+        removedProductId: null
+   },
+
+   mounted() {
+      this.fetchProducts();
+      this.fetchCategories();
+      this.setCurrentPage();
+
+      window.onpopstate = () => {
+         this.setCurrentPage();
+      }
    },
 
    computed: {
@@ -211,17 +47,6 @@ new Vue({
          let start = (this.currentPage - 1) * this.perPage;
          let end = (this.currentPage * this.perPage);
          return this.productsSorted.slice(start, end);
-      },
-
-      categories() {
-         let categories = this.products.map(el => el.category);
-
-         return Array.from(new Set(categories))
-                     .sort((a, b) => {
-                        if (a < b) return -1;
-                        else if (a > b) return 1;
-                        else return 0;
-                     });
       },
 
       productsSorted() {
@@ -278,11 +103,52 @@ new Vue({
       }
    },
    methods: {
+      setPushState(event) {
+         event.preventDefault();
+         const url = event.srcElement.href;
+         if (url) {
+            history.pushState({}, '', url);
+         }
+      },
+
+      setCurrentPage() {
+         let params = new URLSearchParams(window.location.search.slice(1));
+         this.currentPage = params.has('page') ? parseInt(params.get('page')) : 1;
+      },
+
+      fetchProducts() {
+         axios.get('/products')
+               .then(({ data }) => {
+                  this.products = data.data;
+               });
+      },
+
+      fetchCategories() {
+         axios.get('/categories')
+               .then(({ data }) => {
+                  this.categories = data.data;
+               });
+      },
+
       remove(product) {
          if (confirm("Are you sure?")) {
-            let index = this.products.findIndex(item => item.id == product.id);
-            this.products.splice(index, 1);
-         }
+            axios.delete('/products/' + product.id)
+                  .then(res => {
+                     // store the product.id in removedProductId
+                     this.removedProductId = product.id;
+                     
+                     // delay the execution for 1 second
+                     // then set the removedProductId back to null to detach
+                     // the table-danger class from <tr>
+                     // after that remove the tr from UI
+                     new Promise(resolve => setTimeout(resolve, 1000))
+                           .then(() => {
+                              this.removedProductId = null;
+                              let index = this.products.findIndex(item => item.id === product.id);
+                              this.products.splice(index, 1);
+                           })
+                     });
+            }
       },
 
       saveOrUpdate() {
@@ -294,13 +160,23 @@ new Vue({
       },
 
       update() {
-         let index = this.products.findIndex(item => item.id == this.product.id);
-
-         this.products.splice(index, 1, this.product);
-
-         this.isEdit = false;
-
-         $(this.$refs.vuemodal).modal('hide');
+         this.product.price = this.product.price * 100;
+  
+         axios.put('/products/' + this.product.id, this.product)
+            .then(({ data }) => {
+               let index = this.products.findIndex(item => item.id === this.product.id);
+         
+               this.products.splice(index, 1, data.data);
+         
+               this.isEdit = false;
+               
+               this.errors = {};
+         
+               $(this.$refs.vuemodal).modal('hide');
+            })
+            .catch(({ response }) => {
+               this.errors = response.data.errors
+            })
       },
 
       add() {
@@ -325,25 +201,31 @@ new Vue({
       },
 
       save() {
-         if (this.product.name && this.product.category && this.product.price) {
-            this.product.id = this.products.length + 1;
+         this.product.price = this.product.price * 100;
 
-            this.products.unshift(this.product)
+         axios.post('/products', this.product)
+               .then(({ data }) => {
+                  this.productsPaginated.unshift(data.data)
 
-            this.product = {
-               id: null,
-               name: '',
-               category: '',
-               price: ''
-            }
+                  this.product = {
+                     id: null,
+                     name: '',
+                     category: '',
+                     price: ''
+                  }
 
-            $(this.$refs.vuemodal).modal('hide');
-         } else {
-            alert("Please fill in the form properly");
-         }
+                  this.errors = {};
+
+                  $(this.$refs.vuemodal).modal('hide');
+               })
+               .catch(({ response }) => {
+                  this.errors = response.data.errors;
+                  console.log(this.errors)
+               });
       },
 
-      switchPage(page) {
+      switchPage(page, event) {
+         this.setPushState(event);
          this.currentPage = page;
       },
 
@@ -363,13 +245,15 @@ new Vue({
          this.filters.name = '';
       },
 
-      prev() {
+      prev(event) {
+         this.setPushState(event);
          if (!this.isFirstPage) {
             this.currentPage--;
          }
       },
 
-      next() {
+      next(event) {
+         this.setPushState(event);
          if (!this.isLastPage) {
             this.currentPage++;
          }
