f664a3b7d2c9fad5351fdc4ef5327caabfdb0c45
Ives <ivesfurtado@yahoo.com>
2021-02-25 18:40:38 -0300

    Limited answers shown and added load more answers button

--- qa-app/routes/web.php
+++ qa-app/routes/web.php
@@ -27,7 +27,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('questions', QuestionsController::class)->except('show');
-Route::resource('questions.answers', AnswersController::class)->except(['index', 'create', 'show']);
+Route::resource('questions.answers', AnswersController::class)->except(['create', 'show']);
 Route::get('/questions/{slug}', [QuestionsController::class, 'show'])->name('questions.show');
 Route::post('/answers/{answer}/accept', AcceptAnswerController::class)->name('answers.accept');
 
