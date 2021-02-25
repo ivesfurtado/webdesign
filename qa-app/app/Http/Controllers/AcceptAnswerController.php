cf1bfb91716842b10b5707ea417a6adb5ee88be2
Ives <ivesfurtado@yahoo.com>
2021-02-24 22:57:55 -0300

    Changed accept best answer button into Vuejs component

--- qa-app/app/Http/Controllers/AcceptAnswerController.php
+++ qa-app/app/Http/Controllers/AcceptAnswerController.php
@@ -10,6 +10,13 @@ class AcceptAnswerController extends Controller
     public function __invoke(Answer $answer) {
         $this->authorize('accept', $answer);
         $answer->question->acceptBestAnswer($answer);
+
+        if (request()->expectsJson()) {
+            return response()->json([
+                'message' => "Best answer accepted successfully."
+            ]);
+        }
+
         return back();
     }
 }
---
+++ qa-app/resources/js/components/Accept.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <a v-if="canAccept" title="Mark this answer as best answer" 
+            :class="classes"
+            @click.prevent="create">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+        <a v-if="accepted" title="This is the best answer" 
+            :class="classes">
+            <i class="fas fa-check fa-2x"></i>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['answer'],
+
+    data() {
+        return {
+            isBest: this.answer.is_best,
+            id: this.answer.id
+        }
+    },
+
+    computed: {
+        canAccept() {
+            return true;
+        },
+
+        accepted() {
+            return !this.canAccept && this.isBest;
+        },
+
+        classes() {
+            return [
+                'mt-2',
+                this.isBest ? 'vote-accepted' : ''
+                ];
+        },
+
+        endpoint() {
+            return `/answers/${this.id}/accept`;
+        }
+    },
+
+    methods: {
+        create() {
+            axios.post(this.endpoint)
+            .then(response => {
+                this.$toast.success(response.data.message, "Success", {
+                    timeout: 3000,
+                    position: 'bottomLeft'
+                });
+
+                this.isBest = true;
+            })
+        }
+    }
+}
+</script>
